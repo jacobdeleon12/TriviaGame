a07db339ef60cb7839e8977eb8e7fcd1953c9492
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-03-25 17:13:56 -0700

    adding

--- javascript/app.js
+++ javascript/app.js
@@ -4,167 +4,133 @@ var incorrect = 0;
 var nonAnswer = 0;
 var gameRunning = false;
 var myQuestions = [
-    {
-      question: "Who does the opening monologue for every 'Twilight Zone' episode?",
-      answers: [
-        "Orson Welles",
-        "Earl Hamner Jr.",
-        "Rod Serling",
-        "Jack Palance"
-      ],
-      correctAnswer: "Rod Serling"
-    },
+  {
+    question: "Who does the opening monologue for every 'Twilight Zone' episode?",
+    answers: [
+      "Orson Welles",
+      "Earl Hamner Jr.",
+      "Rod Serling",
+      "Jack Palance"
+    ],
+    correctAnswer: "Rod Serling"
+  },
 
-    {
-      question: "Which of the following episodes has NOT been redone by 'The Simpsons'?",
-      answers: [
-        "Nightmare at 20,000 Feet",
-        "A Game of Pool",
-        "It's A Good Life",
-        "Little Girl Lost"
-      ],
-      correctAnswer: "A Game of Pool"
-    }
-  ];
+  {
+    question: "Which of the following episodes has NOT been redone by 'The Simpsons'?",
+    answers: [
+      "Nightmare at 20,000 Feet",
+      "A Game of Pool",
+      "It's A Good Life",
+      "Little Girl Lost"
+    ],
+    correctAnswer: "A Game of Pool"
+  }
+];
 
-  var text = "";
+var text = "";
 var timer;
 var timeInSeconds = 120;
+var j = 0;
 
 function newGame() {
 
-    timeInSeconds = 120;
-    correct = 0;
-    incorrect = 0;
-    nonAnswer = 0;
+  timeInSeconds = 120;
+  correct = 0;
+  incorrect = 0;
+  nonAnswer = 0;
 
 
-    gameRunning = true;
+  gameRunning = true;
 
-    $('#timer').text("Time left 120 ");
+  $('#timer').text("Time left 120 ");
 
-    timer = setInterval(function(){
-      countDown();
-    },1000);
+  timer = setInterval(function () {
+    countDown();
+  }, 1000);
 
-    for (var j = 0; j < myQuestions.length; j++) {
-      console.log(myQuestions.length);
-      
-    $("#domAnswers").append(myQuestions[j].question);
-    $('#domAnswers').append('<br/>'); 
-
+  for (j = 0; j < myQuestions.length; j++) {
+    console.log(myQuestions.length);
+    $("#domAnswers").append("<div id='aWrapper'></div>");
+    
+    $("#aWrapper").append(myQuestions[j].question);
+    $('#aWrapper').append('<br/>');
+    
     // + myQuestions[1].answers
     
     
     //push questions to dom
     for (i = 0; i < 4; i++) {
       //create radio buttons
-      $('#domAnswers').append('<div class="btn"><input type="radio", name="radio_name", class = "radio"  />' + myQuestions[j].answers[i] + '</div>'); 
-      
-      $('#domAnswers').append('<br/>'); 
-    
-    }
+      $('#aWrapper').append('<div class="btn"><input type="radio", name="radio_name", class = "radio"  />' + myQuestions[j].answers[i] + '</div>');
+      $('#aWrapper').append('<br/>');
 
     }
-    $(".btn").on("click", function(){
-       text = $(this).text();
-      console.log(text);
-      
-      
-      })
-    
-    
-    $('#finsh').append("<button id='finshBtn'>Finish</button>");
 
 
-    
+
+  }
+  $(".btn").on("click", function () {
+    text = $(this).text();
+    console.log(text);
+
+
+  })
+
+
+  $('#finsh').append("<button id='finshBtn'>Finish</button>");
+
+
+
 }
 
 //create timer
-function countDown(){
-timeInSeconds --;
-$('#timer').text("Time left " + timeInSeconds);
+function countDown() {
+  timeInSeconds--;
+  $('#timer').text("Time left " + timeInSeconds);
 
-if (timeInSeconds === 0 ) {
-  checkAnswers()
-  $('#timer').text("Times Up");
-  $('#domAnswers').text("");
-  $("#domQuestions").text("");
-  $("#finshBtn").remove();
-  $('#domIncorrect').append('<br/>'); 
-  $("#domIncorrect").append("<button id='startGame2'>Start New Game</button>");
-
-}
-};
-
-
-$(document).on("click", "#startGame", function() {
- //remove game button
-$("#startGame").remove();
-//call new game function
-newGame();
-  });
-
-
-  
-  $(document).on("click", "#finshBtn", function() {
-    //checking all slected answers
+  if (timeInSeconds === 0) {
     checkAnswers();
-    //removing all content from the dom, 
-    $('#timer').text("");
+    $('#timer').text("Times Up");
     $('#domAnswers').text("");
     $("#domQuestions").text("");
     $("#finshBtn").remove();
-    //adding newgame btn
-    $('#domIncorrect').append('<br/>'); 
+    $('#domIncorrect').append('<br/>');
     $("#domIncorrect").append("<button id='startGame2'>Start New Game</button>");
-  });
-  
-  $(document).on("click", "#startGame2", function() {
-  
-      
-    newGame();
-    $("#startGame").remove();
-    $("#domCorrect").text("");
-    $("#domIncorrect").text("");
-  
-  });
+
+  }
+};
+
+
+$(document).on("click", "#startGame", function () {
+  //remove game button
+  $("#startGame").remove();
+  //call new game function
+  newGame();
+});
 
 
 
-  // $("#startGame").on("click", function(){
-  // //remove game button
-  // $("#startGame").remove();
-  // //call new game function
-  // newGame();
+$(document).on("click", "#finshBtn", function () {
+  //checking all slected answers
+  checkAnswers();
+  //removing all content from the dom, 
+  $('#timer').text("");
+  $('#domAnswers').text("");
+  $("#domQuestions").text("");
+  $("#finshBtn").remove();
+  //adding newgame btn
+  $('#domIncorrect').append('<br/>');
+  $("#domIncorrect").append("<button id='startGame2'>Start New Game</button>");
+});
 
-  // $("#finshBtn").on("click" , function(){
-    //   //checking all slected answers
-//   checkAnswers();
-//   //removing all content from the dom, 
-//   $('#domAnswers').text("");
-//   $("#domQuestions").text("");
-//   $("#finshBtn").remove();
-//   //adding newgame btn
-//   $('#domIncorrect').append('<br/>'); 
-//   $("#domIncorrect").append("<button id='startGame2'>Start New Game</button>");
-  
-  
+$(document).on("click", "#startGame2", function () {
 
+  newGame();
+  $("#startGame").remove();
+  $("#domCorrect").text("");
+  $("#domIncorrect").text("");
 
-//   $("#startGame2").on("click", function(){
-//     newGame();
-//     $("#startGame").remove();
-//     $("#domCorrect").text("");
-//     $("#domIncorrect").text("");
-
-// });
-// });
-// });
-
-
-
-
+});
 
 
 //check answers
@@ -172,31 +138,30 @@ function checkAnswers() {
 
   clearInterval(timer);
 
-  if (text === myQuestions[0].correctAnswer) {
-      gameRunning = false;
-      correct++;
-      
-      $("#domCorrect").html(("You answered: " + correct + " correct!" ))
-      
-    
+  if (text === myQuestions[0].correctAnswer ) {
+    // || myQuestions[1].correctAnswer
+    gameRunning = false;
+    correct++;
+
+    $("#domCorrect").html(("You answered: " + correct + " Correct!"));
+
+
   }
-  else if(text != myQuestions[0].answers){
+  else if (text != myQuestions[0].answers) {
+    // || myQuestions[1].answers
     gameRunning = false;
     incorrect++
-    
-    $("#domIncorrect").html("You answered: " + incorrect + " incorrect!" )
-    
+
+    $("#domIncorrect").html("You answered: " + incorrect + " Incorrect!");
+
   }
-  else{
+  else {
     //not working
     gameRunning = false;
     nonAnswer++;
 
-    $("#domIncorrect").html("You answered: " + nonAnswer + " incorrect!" )
+    $("#domIncorrect").html("You didnt answer: " + nonAnswer  );
     //questions you didnt answer
   }
 }
 
-//clear dom and display correct and incorrect counter
-
-//
