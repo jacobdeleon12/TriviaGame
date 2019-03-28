15332c0861ad6b90e84f469f4142fc28defb9029
Jacob DeLeon <jacobdeleon12@gmail.com>
2019-03-27 22:44:18 -0700

    adding

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,7 @@
 var correct = 0;
 var incorrect = 0;
 var nonAnswer = 0;
-var gameRunning = false;
+
 var myQuestions = [
   {
     question: "Who does the opening monologue for every 'Twilight Zone' episode?",
@@ -24,7 +24,40 @@ var myQuestions = [
       "Little Girl Lost"
     ],
     correctAnswer: "A Game of Pool"
-  }
+  },
+
+  {
+    question: "How many seasons did the ORIGINAL 'Twilight Zone' air?",
+    answers: [
+      "4",
+      "7",
+      "5",
+      "2"
+    ],
+    correctAnswer: "A Game of Pool"
+  },
+
+  {
+    question: "In 'The Hitch-Hiker', who is the hitch-hiker?",
+    answers: [
+      "Life",
+      "Death",
+      "Happiness",
+      "Just Some Bum"
+    ],
+    correctAnswer: "Death"
+  },
+
+  {
+    question: "Which of these is NOT an hour long episode?",
+    answers: [
+      "Miniature",
+      "A Passage for Trumpet",
+      " The New Exhibit",
+      "On Thursday We Leave For Home"
+    ],
+    correctAnswer: "A Passage for Trumpet"
+  },
 ];
 
 var text = "";
@@ -40,35 +73,38 @@ function newGame() {
   nonAnswer = 0;
 
 
-  gameRunning = true;
 
+  //setting timer text to 120 so when the page loads it dosnt look weird
   $('#timer').text("Time left 120 ");
-
+  //timer function
   timer = setInterval(function () {
     countDown();
   }, 1000);
 
+  var val = 0;
+  //loop for questions
   for (j = 0; j < myQuestions.length; j++) {
-    console.log(myQuestions.length);
+
     $("#domAnswers").append("<div id='aWrapper'></div>");
-    
+
     $("#aWrapper").append(myQuestions[j].question);
     $('#aWrapper').append('<br/>');
-    
-    // + myQuestions[1].answers
-    
-    
-    //push questions to dom
+
+
+
+
+    //loop for answers
     for (i = 0; i < 4; i++) {
       //create radio buttons
-      $('#aWrapper').append('<div class="btn"><input type="radio", name="radio_name", class = "radio"  />' + myQuestions[j].answers[i] + '</div>');
+      $('#aWrapper').append('<div class="btn"><input type="radio", name="' + val + '", class = "radio"  />' + myQuestions[j].answers[i] + '</div>');
       $('#aWrapper').append('<br/>');
 
     }
-
+    val++;
 
 
   }
+  //on click function
   $(".btn").on("click", function () {
     text = $(this).text();
     console.log(text);
@@ -76,7 +112,7 @@ function newGame() {
 
   })
 
-
+  //add finish button
   $('#finsh').append("<button id='finshBtn'>Finish</button>");
 
 
@@ -100,7 +136,7 @@ function countDown() {
   }
 };
 
-
+//click for new game
 $(document).on("click", "#startGame", function () {
   //remove game button
   $("#startGame").remove();
@@ -109,7 +145,7 @@ $(document).on("click", "#startGame", function () {
 });
 
 
-
+//click for finish btn
 $(document).on("click", "#finshBtn", function () {
   //checking all slected answers
   checkAnswers();
@@ -120,15 +156,21 @@ $(document).on("click", "#finshBtn", function () {
   $("#finshBtn").remove();
   //adding newgame btn
   $('#domIncorrect').append('<br/>');
-  $("#domIncorrect").append("<button id='startGame2'>Start New Game</button>");
+  $('#domNonAnswer').append('<br/>');
+
+  $("#domNonAnswer").append("<button id='startGame2'>Start New Game</button>");
 });
 
+
+//click for new game btn after first game
 $(document).on("click", "#startGame2", function () {
 
   newGame();
   $("#startGame").remove();
   $("#domCorrect").text("");
   $("#domIncorrect").text("");
+  $("#domNonAnswer").text("");
+
 
 });
 
@@ -138,30 +180,47 @@ function checkAnswers() {
 
   clearInterval(timer);
 
-  if (text === myQuestions[0].correctAnswer ) {
-    // || myQuestions[1].correctAnswer
-    gameRunning = false;
-    correct++;
 
-    $("#domCorrect").html(("You answered: " + correct + " Correct!"));
+  for (var i = 0; i < myQuestions.length; i++) {
 
 
+
+    // not working after finding first answer
+
+    if (text === myQuestions[i].correctAnswer && text != "") {
+      // || myQuestions[1].correctAnswer
+
+      correct++;
+
+      console.log(myQuestions[i]);
+
+
+
+    }
+
+    else if (text != myQuestions[i].correctAnswer && text != "") {
+      // || myQuestions[1].answers
+
+      incorrect++
+
+      console.log(myQuestions[i]);
+
+
+    }
+    else {
+      //not working
+
+      nonAnswer++;
+
+
+      //questions you didnt answer
+      console.log(myQuestions[i]);
+
+    }
   }
-  else if (text != myQuestions[0].answers) {
-    // || myQuestions[1].answers
-    gameRunning = false;
-    incorrect++
+  $("#domCorrect").html(("You answered: " + correct + " Correct!"));
+  $("#domIncorrect").html("You answered: " + incorrect + " Incorrect!");
+  $("#domNonAnswer").html("You didnt answer: " + nonAnswer);
 
-    $("#domIncorrect").html("You answered: " + incorrect + " Incorrect!");
-
-  }
-  else {
-    //not working
-    gameRunning = false;
-    nonAnswer++;
-
-    $("#domIncorrect").html("You didnt answer: " + nonAnswer  );
-    //questions you didnt answer
-  }
 }
 
